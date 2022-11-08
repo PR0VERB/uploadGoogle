57813d88974a2cc4bfb9197e2dc2017d7eaed065
PR0VERB <36727013+PR0VERB@users.noreply.github.com>
2022-11-08 18:45:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,87 +1,135 @@
-import base64
-import datetime
-import io
+# https://www.youtube.com/watch?v=aruInGd-m40&t=278s
+# https://www.youtube.com/watch?v=H8Ars15wGRM
 
-import dash
-from dash.dependencies import Input, Output, State
-from dash import dcc, html, dash_table
-
+import streamlit as st
 import pandas as pd
+import time
+import datetime
+import streamlit.components.v1 as components
+from PIL import Image
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+######################### Import UPLOADING Libraries ####################
+# https://www.youtube.com/watch?v=fkWM7A-MxR0
+import os
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload   
+######################### Import UPLOADING Libraries ####################
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div([
-    dcc.Upload(
-        id='upload-data',
-        children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        # Allow multiple files to be uploaded
-        multiple=True
-    ),
-    html.Div(id='output-data-upload'),
-])
+# initialise stakeholder dataframe
 
-def parse_contents(contents, filename, date):
-    content_type, content_string = contents.split(',')
+stake_df= pd.read_excel('G:/My Drive/GOOGLEDRIVE_CODE/DATA/stakes_df.xlsx')
+stake_addresses = list(stake_df['Email Address'])
 
-    decoded = base64.b64decode(content_string)
+# verify email
+enter_email = st.text_input("Please enter your email address", value="")
+
+if enter_email not in stake_addresses:
+    st.write('It seems like your email address does not have access to this plaform. Please contact Bill - bseota@gmail.com')
+else:
+    st.write('Email verified')
+
+    drive_directory_df = pd.read_excel('G:/My Drive/GOOGLEDRIVE_CODE/DATA/GoogleDriveIndicatorMetroFolderID.xlsx', sheet_name='main')
+    # metro list
+    metros = list(drive_directory_df.Metro.unique())
+    metros.insert(0,'Make a Selection')
+    # indicator list
+    indicators = list(drive_directory_df.Indicator.unique())
+    indicators.insert(0,'Make a Selection')
+
+    metro_selected = st.selectbox("Select a metro", tuple(metros),)
+    if metro_selected != 'Make a Selection':
+        indicator_selected = st.selectbox("Select an indicator", tuple(indicators))
+    
+    uploaded_file = st.file_uploader("Choose an xlsx or csv file")
+    print("UPLOADED FILE ##############################", uploaded_file)
+
+    # TODO: make file type agnostic
+    def chosen_sheet(uploaded_file: str):
+       return pd.read_excel(uploaded_file,sheet_name = 'Sheet1')
+
+    #TODO Columns validation 
+
+    if uploaded_file is not None:
+        df_ = chosen_sheet(uploaded_file)
+        
+                                                            ######################### UPLOADING #########################
+
+    SCOPES = ['https://www.googleapis.com/auth/drive'] # if we want to change the scope then we need to delete the current token and create a new one
+
+    creds = None
+    # retreive folder id of the appropriate metro & indicator
+        # for some reason, the below works without the .FolderID.values[0]
+    folder_id = drive_directory_df[(drive_directory_df.Metro == metro_selected) & (drive_directory_df.Indicator == indicator_selected)].FolderID.values[0] 
+    
+    if os.path.exists("token.json"): #if the token exists 
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+        
+    if not creds or not creds.valid:
+        #in the case of creds not valid
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            # create app flow from credentials file 
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES)
+            # run
+            
+            creds = flow.run_local_server(port=0)
+            
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+            
     try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
-        elif 'xls' in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
+        service = build("drive", "v3", credentials = creds)
+        #### temporary solution to upload since we are using google drive and not an actual database ####
+        
+        # 1. get the current working directory
+        file_dir = os.getcwd()
+        # 2. append current working directory to the file name
+        file_path = os.path.join(file_dir,uploaded_file.name)
+        # 3. take the dataframe to excel and save to file directory 
+        df_.to_excel(file_path)
+        del df_
+        # 4. upload the file with name file_path to drive
+        file_metadata = {
+            "name": uploaded_file.name,
+            "parents": [folder_id]
+        }
+        print("FILE PATH IS " + file_path)
+        print("FILE type IS ", type(file_path))
+        
+        print("folder PATH IS " + str(folder_id))
+        print("folder type IS ", type(folder_id))
+        
+        media = MediaFileUpload(file_path)
+        print(type(media))
+        upload_file = service.files().create(
+            body = file_metadata, fields = "id", media_body = media).execute()
+        
+        # media = MediaFileUpload(filename =  "G:/My Drive/GOOGLEDRIVE_CODE/CODE/STREAMLIT/UPLOADFOLDER/backupfiles/fe.txt") # , mimetype='application/vnd.ms-excel' , resumable=True
+        # print(media)
+        
+        # upload_drive_file = service.files().create(body = file_metadata, fields = "id", media_body = media)
+        # upload_drive_file.execute()
+        
+        print("Backed up file" + uploaded_file.name)
+        
+    except HttpError as e:
+        print("Error:" + str(e))        
+        # 5. delete file in cwd
+        
+        #### temporary solution to upload since we are using google drive and not an actual database ####
+        
+       
+        
 
-    return html.Div([
-        html.H5(filename),
-        html.H6(datetime.datetime.fromtimestamp(date)),
+        
 
-        dash_table.DataTable(
-            df.to_dict('records'),
-            [{'name': i, 'id': i} for i in df.columns]
-        ),
 
-        html.Hr(),  # horizontal line
-
-        # For debugging, display the raw contents provided by the web browser
-        html.Div('Raw Content'),
-        html.Pre(contents[0:200] + '...', style={
-            'whiteSpace': 'pre-wrap',
-            'wordBreak': 'break-all'
-        })
-    ])
-
-@app.callback(Output('output-data-upload', 'children'),
-              Input('upload-data', 'contents'),
-              State('upload-data', 'filename'),
-              State('upload-data', 'last_modified'))
-def update_output(list_of_contents, list_of_names, list_of_dates):
-    if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
-        return children
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+                                                        ######################### end UPLOADING #########################
